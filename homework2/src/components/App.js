888de4b0caaee0132ff17ca8d356e09f222d77a5
Jenya Hutsul <79422984+JenyaHutsul@users.noreply.github.com>
2022-06-13 15:35:17 +0300

    Update App.js

--- homework2/src/components/App.js
+++ homework2/src/components/App.js
@@ -31,13 +31,5 @@ class App extends React.Component {
     }
 }
 
-//(item,index) => {
-//    <li key={index}>
-//        <div className="card-body">
-//            <h5 className="card-title">{item.title}</h5>
-//            <p className="card-text">{item.text}</p>
-//        </div>
-//    </li>
-//}
 
 export default App;
